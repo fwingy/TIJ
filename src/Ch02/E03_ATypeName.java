8f6f234cabfa6a738a50b4a80f5f7cf164a08853
fwingy <lzzi386@outlook.com>
2017-02-27 11:53:33 +0800

    172/27

---
+++ src/Ch02/E03_ATypeName.java
@@ -0,0 +1,12 @@
+//: object/E03_ATypeName.java
+/****************** Exercise 3 ******************
+* Turn the code fragments involving ATypeName
+* into a program that compiles and
+* runs.
+************************************************/
+package Ch02;
+public class E03_ATypeName {
+public static void main(String[] args) {
+	E03_ATypeName a = new E03_ATypeName();
+}
+} ///:~
